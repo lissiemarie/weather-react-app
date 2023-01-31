1ffa93458030322c1c45d3d22da446005cc713d5
lissiemarie <elyssamunden@gmail.com>
2023-01-31 06:47:40 -0800

    init for Weather React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Weather from "./Weather";
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from Weather!</h1>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import { TailSpin } from 'react-loader-spinner';
+
+export default function Weather() {
+
+    function handleResponse(response) {
+        alert(`The weather in ${city} is ${Math.round(response.data.main.temp)}°F`)
+    }
+
+    let apiKey = "40dfbc5bab159e395434bbfed3c3f9ef";
+    let city = "Dallas";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return (<TailSpin
+    height = "80"
+    width = "80"
+    radius = "1"
+    color = 'green'
+    ariaLabel = 'tail-spin-loading'     
+    wrapperStyle={{}}
+    wrapperClass=""
+    visible={true}
+  />);
+}
